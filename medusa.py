53c567d305f1735b9356879883383cc9a7dbb62e
zafernur <77587405+zafernur@users.noreply.github.com>
2024-05-22 14:17:47 +0200

    New functions for a different plotting way
    Two new functions added. The first one takes a group of CSV files that contain the same type of data with the same column structure and puts the data from each CSV file in a dictionary as a pandas dataframe. The user can give keys as a tag for each dataframe and column titles for each dataframe as function arguments. The second function is the second version of the plotting, function plot_sepctrum. The new version, plot_spectrumv2, takes a dictionary consist of dataframes with tags, and can plot any data list from those dataframes.

--- medusa.py
+++ medusa.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from datetime import datetime
 import os
+from typing import Union, List
 
 def create_list_of_files(extension, directory=os.getcwd()):
     """
@@ -132,7 +133,37 @@ def convert_probs_to_counts(csv_files, particle_per_second, live_time):
         df['Counts'] = df['Broadenings'] * particle_per_second * live_time
         df.to_csv(file, index=False, header=False, sep='\t')
 
-def plot_spectrum(x, y, labels, savefig_name, title, xlabel, ylabel, yscale='linear', plot_type='scatter', **kwargs):
+def csv_to_dfs(csv_files: Union[str, List[str]], *args, titels=['Energies', 'Probs', 'Errors', 'Broadened', 'Counts']):
+    """
+    Creates dataframe(s) from a given csv file(s). The csv files must be in the same column format
+    and contain same type of data.
+
+    Args:
+        csv_files (str or list): String or list of strings that specifies the csv files
+                                 from which dataframes will be created. If user creates only
+                                 one dataframe but still wants to store it in a dictionary than
+                                 it must be given as a list with only one element.
+        titels (list): List of strings that specifies the column titels of the dataframes.
+        *args (str or list): String or list of strings that specifies the names of the dataframes
+                             as keys of a dictionary. If it is not specified the names will be
+                             df1, df2, etc. If the user creates only one dataframe but still wants
+                             to store it in a dictionary than it must be given as a string.
+    """
+    if isinstance(csv_files, str):
+        df = pd.read_csv(csv_files, sep='\t', header=None, names=titels)
+        return df
+    elif isinstance(csv_files, list):
+        dfs = {}
+        for i, file in enumerate(csv_files, start=1):
+            if not args:
+                df_name = f"df{i}"
+                dfs[df_name] = pd.read_csv(file, sep='\t', header=None, names=titels)
+            else:
+                dfs[args[i-1]] = pd.read_csv(file, sep='\t', header=None, names=titels)
+        return dfs
+
+def plot_spectrum(x, y, labels, savefig_name, title, xlabel, ylabel, colors=None, fig_size=(8,4), x_lims=False, y_lims=False,
+                  yscale='linear', plot_type='scatter', grid_major=False, grid_minor=False, **kwargs):
     """
     Plots the spectra obtained from MCNP simulations in one plot, on top of each other.
 
@@ -147,39 +178,160 @@ def plot_spectrum(x, y, labels, savefig_name, title, xlabel, ylabel, yscale='lin
         title (str): For title of the plot.
         xlabel (str): For the label of the x-axis.
         ylabel (str): For the label of the y-axis.
+        colors (list): The list of colors for lines of the plot. Order of the list must be correspond to
+                       the order of the data in y.
+        x_lims (list): A list with two element as lowest and highest limits for x-axis. Default: False
+        y_lims (list): A list with two element as lowest and highest limits for y-axis. Default: False
         yscale (str): For the scale type of the y-axis. It can be linear, log, symlog, or logit.
                       The default value is linear.
         plot_type (str): The type of the plot. It can be scatter, stackplot, line, or bar. The
                          default value is scatter.
+        grid_major (bool): To put major grid lines. Default is Flase.
+        grid_minor (bool): To put minor grid lines. Default is Flase.
         **kwargs (Any): All the line properties from matplotlib can be used in the same way here.
     
     Return:
         Axes: It saves the plot in a png file and shows it on your screen.
     """
-    fig, ax = plt.subplots(1, 1, dpi=200)
+    fig, ax = plt.subplots(1, 1, figsize=fig_size, dpi=200)
     if plot_type == 'scatter':
         for i in range(len(y)):
-            ax.scatter(x, y[i], **kwargs)
+            if colors:
+                ax.scatter(x, y[i], color=colors[i], **kwargs)
+            else:
+                ax.scatter(x, y[i], **kwargs)
     elif plot_type == 'stackplot':
         for i in range(len(y)):
-            ax.stackplot(x, y[i], **kwargs)
+            if colors:
+                ax.stackplot(x, y[i], color=colors[i], **kwargs)
+            else:
+                ax.stackplot(x, y[i], **kwargs)
     elif plot_type == 'line':
         for i in range(len(y)):
-            ax.line(x, y[i], **kwargs)
+            if colors:
+                ax.line(x, y[i], color=colors[i], **kwargs)
+            else:
+                ax.line(x, y[i], **kwargs)
     elif plot_type == 'bar':
         for i in range(len(y)):
-            ax.bar(x, y[i], **kwargs)
+            if colors:
+                ax.bar(x, y[i], color=colors[i], **kwargs)
+            else:
+                ax.bar(x, y[i], **kwargs)
     else:
         raise ValueError("Invalid plot type. Choose from 'scatter', 'stackplot',  'line', or 'bar'.")
     
     ax.set_title(title)
     ax.set_xlabel(xlabel)
     ax.set_ylabel(ylabel)
-    ax.set_xlim(0)
+    ax.set_xlim(x_lims)
+    ax.set_ylim(y_lims)
     ax.set_yscale(yscale)
     ax.minorticks_on()
     ax.tick_params(which='both', direction='in', top=True, right=True)
+    ax.set_axisbelow(True)
+    if grid_major:
+        ax.grid(which='major', color='#DDDDDD', linewidth=0.8)
+    else: ax.grid(visible=False, which='major')
+    if grid_minor:
+        ax.grid(which='minor', color='#DDDDDD', linestyle=':', linewidth=0.6)
+    else: ax.grid(visible=False, which='minor')
     if len(y) == 1:
+        plt.savefig(savefig_name, edgecolor='none', transparent=True)
+    else:
+        ax.legend([label for label in labels], fontsize=7, fancybox=False,
+                  framealpha= 0.0, facecolor='inherit')
+        plt.savefig(savefig_name, edgecolor='none', transparent=True)
+    return ax
+
+def plot_spectrumv2(dfs, which_dataframes, column_name_for_x, column_name_for_y, start_x, end_x,
+                    labels, savefig_name, title, xlabel, ylabel, colors=None, fig_size=(8,4), x_lims=False, y_lims=False,
+                    yscale='linear', plot_type='scatter', grid_major=False, grid_minor=False, **kwargs):
+    """
+    Plots the spectra obtained from MCNP simulations in one plot, on top of each other.
+
+    Args:
+        dfs (dictionary): Contains dataframe names as keys and dataframes that obtained from each csv files as values.
+        which_dataframes (list): List of keys from dfs dictionary that specifies which dataframe(s) will be plotted.
+        column_name_for_x (str): The column that will be used as x data of the plot.
+        column_name_for_y (str): The column that will be used for y data of the plot. Just one column name is enough
+                                 because it will exist in every dataframe that is to be plotted.
+        start_x (int): Starting data point for x-axis. It has to be the index corresponding to that data point.
+        end_x (int): The last data point for x-axis. It has to be the index corresponding to that data point.
+        labels (list): The list of labels to put on the legend of the plot.
+        savefig_name (str): String that specifies the name of the output file as png.
+        title (str): For title of the plot.
+        xlabel (str): For the label of the x-axis.
+        ylabel (str): For the label of the y-axis.
+        colors (list): The list of colors for lines of the plot. Order of the list must be correspond to
+                       the order of the data in y.
+        x_lims (list): A list with two element as lowest and highest limits for x-axis. Default: False
+        y_lims (list): A list with two element as lowest and highest limits for y-axis. Default: False
+        yscale (str): For the scale type of the y-axis. It can be linear, log, symlog, or logit.
+                      The default value is linear.
+        plot_type (str): The type of the plot. It can be scatter, stackplot, line, or bar. The
+                         default value is scatter.
+        grid_major (bool): To put major grid lines. Default is Flase.
+        grid_minor (bool): To put minor grid lines. Default is Flase.
+        **kwargs (Any): All the line properties from matplotlib can be used in the same way here.
+    
+    Return:
+        Axes: It saves the plot in a png file and shows it on your screen.
+    """
+
+    fig, ax = plt.subplots(1, 1, figsize=fig_size, dpi=200)
+
+    if plot_type == 'scatter':
+        for i in range(len(which_dataframes)):
+            if colors:
+                ax.scatter(dfs[which_dataframes[0]][column_name_for_x][start_x:end_x],
+                           dfs[which_dataframes[i]][column_name_for_y][start_x:end_x], color=colors[i], **kwargs)
+            else:
+                ax.scatter(dfs[which_dataframes[0]][column_name_for_x][start_x:end_x],
+                           dfs[which_dataframes[i]][column_name_for_y][start_x:end_x], **kwargs)
+    elif plot_type == 'stackplot':
+        for i in range(len(which_dataframes)):
+            if colors:
+                ax.stackplot(dfs[which_dataframes[0]][column_name_for_x][start_x:end_x],
+                           dfs[which_dataframes[i]][column_name_for_y][start_x:end_x], color=colors[i], **kwargs)
+            else:
+                ax.stackplot(dfs[which_dataframes[0]][column_name_for_x][start_x:end_x],
+                           dfs[which_dataframes[i]][column_name_for_y][start_x:end_x], **kwargs)
+    elif plot_type == 'line':
+        for i in range(len(which_dataframes)):
+            if colors:
+                ax.line(dfs[which_dataframes[0]][column_name_for_x][start_x:end_x],
+                           dfs[which_dataframes[i]][column_name_for_y][start_x:end_x], color=colors[i], **kwargs)
+            else:
+                ax.line(dfs[which_dataframes[0]][column_name_for_x][start_x:end_x],
+                           dfs[which_dataframes[i]][column_name_for_y][start_x:end_x], **kwargs)
+    elif plot_type == 'bar':
+        for i in range(len(which_dataframes)):
+            if colors:
+                ax.bar(dfs[which_dataframes[0]][column_name_for_x][start_x:end_x],
+                           dfs[which_dataframes[i]][column_name_for_y][start_x:end_x], color=colors[i], **kwargs)
+            else:
+                ax.bar(dfs[which_dataframes[0]][column_name_for_x][start_x:end_x],
+                           dfs[which_dataframes[i]][column_name_for_y][start_x:end_x], **kwargs)
+    else:
+        raise ValueError("Invalid plot type. Choose from 'scatter', 'stackplot',  'line', or 'bar'.")
+    
+    ax.set_title(title)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    ax.set_xlim(x_lims)
+    ax.set_ylim(y_lims)
+    ax.set_yscale(yscale)
+    ax.minorticks_on()
+    ax.tick_params(which='both', direction='in', top=True, right=True)
+    ax.set_axisbelow(True)
+    if grid_major:
+        ax.grid(which='major', color='#DDDDDD', linewidth=0.8)
+    else: ax.grid(visible=False, which='major')
+    if grid_minor:
+        ax.grid(which='minor', color='#DDDDDD', linestyle=':', linewidth=0.6)
+    else: ax.grid(visible=False, which='minor')
+    if len(which_dataframes) == 1:
         plt.savefig(savefig_name, edgecolor='none', transparent=True)
     else:
         ax.legend([label for label in labels], fontsize=7, fancybox=False,
